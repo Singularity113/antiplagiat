5b5ea073d604eebb894f904b958eb1786e0d3815
Никита <98203466+qTdnkfxNZAhJh4J@users.noreply.github.com>
2023-03-16 19:46:59 +0300

    Add files via upload

--- Antiplagiat.py
+++ Antiplagiat.py
@@ -69,11 +69,15 @@ class MainWindow(QMainWindow):
         self.fileMenu = QMenu("Выбрать папку", self)
         self.menuBar.addMenu(self.fileMenu)
         self.newAction = QAction(self)
-        self.openAction = QAction("Выбрать", self)
+        self.openAction = QAction("Выбрать папку для проверки на уникальность", self)
         self.fileMenu.addAction(self.openAction)
         self.openAction.triggered.connect(self.getDirectory)
         
-    def getDirectory(self):                                                    
+    def getDirectory(self):
+        stop_words = ['я','ты','он','она','они','мы','вы','оно','и','что-то',
+                'а','но','да','или','либо','ни–ни','то–то','что','чтобы','как','потому',
+                'что','так','как','если','дабы','когда','хотя','бы','пусть','будто',
+                'словно','точно','у','о','или','то','да','кто-то']                                                    
         dirlist = QFileDialog.getExistingDirectory(self,"Выбрать папку",".")
         if dirlist !='':
             self.plainTextEdit.appendHtml("<br>Выбрали папку: <b>{}</b>".format(dirlist))
@@ -93,7 +97,46 @@ class MainWindow(QMainWindow):
         for i in range(3):
             self.s_1 = pairs[i][0]
             self.s_2 = pairs[i][1]
-            print(self.s_1, self.s_2)
+            self.s_1 = re.sub('[^а-я-0-9- ]', '', self.s_1) # Убирает из текста все кроме цифр, букв, и "-"
+            self.s_2 = re.sub('[^а-я-0-9- ]', '', self.s_2)
+            self.s_1 = " ".join([word for word in self.s_1.split() if word not in stop_words]) # Убирает из текста стоп-слова
+            self.s_2 = " ".join([word for word in self.s_2.split() if word not in stop_words])
+            self.words_1 = self.s_1.split() # Делит текст 
+            self.words_2 = self.s_2.split()
+            self.text_1 = [] # Создает список
+            self.text_2 = []
+            for word in self.words_1:
+                self.text_1.append(word) # Записывает отдельные слова в список
+            for word in self.words_2:
+                self.text_2.append(word)
+            shingleLen = 2 # Длина шага в шингле
+            self.out_1 = []
+            self.out_2 = []
+            for i in range(len(self.text_1) - (shingleLen - 1)):
+                shingle_1 = [x for x in self.text_1[i:i + shingleLen]]
+                self.out_1.append(shingle_1) # Записывает шинглы по 2 слова с нахлестом в 1 слово в список
+            for i in range(len(self.text_2) - (shingleLen - 1)):
+                shingle_2 = [x for x in self.text_2[i:i + shingleLen]]
+                self.out_2.append(shingle_2)
+                # Хэширование
+            self.hash_1 = [] 
+            self.hash_2 = []
+            for el in self.out_1:
+                self.hash_1.append(binascii.crc32(' '.join(el).encode('utf-8'))) # Записывает шинглы после кодировки в список
+            for el in self.out_2:
+                self.hash_2.append(binascii.crc32(' '.join(el).encode('utf-8')))
+                # Алгоритм проверки
+            self.count = 0 # Счетчик
+            for i in range(len(self.hash_1)):
+                for j in range(len(self.hash_2)):
+                    if self.hash_1[i] == self.hash_2[j]:
+                        self.count += 1
+                        break # Если хэши совпадают увеличиваем счетчик и переходим к след. хэшу
+            if self.count > len(self.hash_1): # Если счетчик больше длины 1-го хэша результат = 100
+                self.result = 100
+            else:
+                self.result = (self.count/len(self.hash_1)) * 100
+            print(self.result)
 
     def slot_btn_first(self): # Функция для выбора 1-го файла
         self.first_content.clear() # Очищаем содержимое поля для ввода 1-го файла
